e97e23310446af809bbdcccc6929b31368dcd628
Gabriel F. <80858878+Trighy@users.noreply.github.com>
2025-06-26 09:32:24 -0300

    Testando ListaSequencial Main.java

--- Avaliacao2/Main.java
+++ Avaliacao2/Main.java
@@ -1,15 +1,27 @@
+import esd.ListaSequencial;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ListaSequencial<String> l = new ListaSequencial<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        l.adiciona("um");
+        l.adiciona("dois");
+        l.adiciona("tres");
+        l.adiciona("quatro");
+        l.adiciona("cinco");
+
+        for (int j=0; j < l.comprimento(); j++) {
+            System.out.println(l.obtem(j));
+        }
+
+        l.inverte();
+        System.out.println("Após inverter: ");
+        for (int j=0; j < l.comprimento(); j++) {
+            System.out.println(l.obtem(j));
         }
     }
 }

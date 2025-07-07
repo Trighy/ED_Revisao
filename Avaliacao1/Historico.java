7ee619b6910eb18c0a4dd8a01394c0df01e2f5e2
Gabriel F. <80858878+Trighy@users.noreply.github.com>
2025-07-07 20:09:31 -0300

    Update Historico.java

--- Avaliacao1/Historico.java
+++ Avaliacao1/Historico.java
@@ -1,27 +1,40 @@
 import esd.Pilha;
+
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Historico {
     public static void main(String[] args) {
-        esd.Pilha<String> h = new esd.Pilha<>();
+        // Cria-se uma Pilha que usaremos para armazenar os comandos.
+        Pilha<String> h = new Pilha<>();
+        // Ira ser utiliza-do para detectar oque foi escrito pelo usuário/programador.
         Scanner programador = new Scanner(System.in);
-        Historico hist = new Historico();
-        for (int i = 0; i < 101; i++) {
+        // Repete o programa infinitamente.
+        for (int i = 0; i > -1; i++) {
+            // Adicionar o "> " no começo do programa
             System.out.print("> ");
+            // Detecta oque o usuário/programador de fato escreveu.
             String comando = programador.next();
+            // Empilha oque foi escrito pelo usuário/programador.
             h.empilha(comando);
+            // Confirma o comando do usuário ter sido executado.
             System.out.println("comando executado");
-            if (Objects.equalsIgnoreCase(comando, "historico")) {
+            // Se o comando que o usuário/programador escreveu for equivante á "historico" mostra-ra os últimos comandos escritos.
+            if (Objects.equals(comando, "historico")) {
                 System.out.println(h.desempilha());
                 System.out.println(h.desempilha());
                 System.out.println(h.desempilha());
                 System.out.println(h.desempilha());
                 System.out.println(h.desempilha());
+                // Empilha o próprio comando "historico".
                 h.empilha("historico");
+                h.empilha(comando);
+                h.empilha(comando);
+                h.empilha(comando);
+                h.empilha(comando);
             }
-            if (Objects.equalsIgnoreCase(comando, "sair")) {
+            // Se o comando que o usuário/programador escreveu for equivante á "sair" o programa é parado.
+            if (Objects.equals(comando, "sair")) {
                 return;
             }
         }
